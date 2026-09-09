add8940f75b95e23a8745633e9d52b9b5a919cfa
Rithwik <trixterzmusic@gmail.com>
2026-09-10 03:05:49 +0530

    test: expand parser coverage

--- tests/unit/ParserTests.cpp
+++ tests/unit/ParserTests.cpp
@@ -118,3 +118,129 @@ GS_TEST(ParserTests, SyntaxErrorDiagnostics) {
     GS_ASSERT(idExpr != nullptr);
     GS_ASSERT_EQ(idExpr->getName(), "abc");
 }
+
+GS_TEST(ParserTests, ExpressionPrecedence) {
+    std::string source =
+        "set value = 2 + 3 * 4\n";
+
+    Lexer lexer(source, "precedence.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+    GS_ASSERT_EQ(program->getStatements().size(), 1);
+
+    auto* decl = dynamic_cast<VarDeclStmt*>(program->getStatements()[0].get());
+    GS_ASSERT(decl != nullptr);
+
+    auto* addExpr = dynamic_cast<BinaryExpr*>(decl->getInitializer());
+    GS_ASSERT(addExpr != nullptr);
+    GS_ASSERT_EQ(addExpr->getOp(), BinaryOp::Add);
+
+    auto* rightExpr = dynamic_cast<BinaryExpr*>(addExpr->getRight());
+    GS_ASSERT(rightExpr != nullptr);
+    GS_ASSERT_EQ(rightExpr->getOp(), BinaryOp::Multiply);
+}
+
+GS_TEST(ParserTests, BooleanLogicAndNot) {
+    std::string source =
+        "if not enemy nearby and health low:\n"
+        "    player defend\n";
+
+    Lexer lexer(source, "logic.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+    GS_ASSERT_EQ(program->getStatements().size(), 1);
+
+    auto* ifStmt = dynamic_cast<IfStmt*>(program->getStatements()[0].get());
+    GS_ASSERT(ifStmt != nullptr);
+
+    auto* andExpr = dynamic_cast<BinaryExpr*>(ifStmt->getCondition());
+    GS_ASSERT(andExpr != nullptr);
+    GS_ASSERT_EQ(andExpr->getOp(), BinaryOp::And);
+
+    auto* notExpr = dynamic_cast<UnaryExpr*>(andExpr->getLeft());
+    GS_ASSERT(notExpr != nullptr);
+    GS_ASSERT_EQ(notExpr->getOp(), UnaryOp::Not);
+
+    auto* rightSensor = dynamic_cast<SensoryConditionExpr*>(andExpr->getRight());
+    GS_ASSERT(rightSensor != nullptr);
+    GS_ASSERT_EQ(rightSensor->getSensoryType(), SensoryType::HealthLow);
+}
+
+GS_TEST(ParserTests, SensorExpressions) {
+    std::string source =
+        "if obstacle ahead:\n"
+        "    player move backward 1\n"
+        "if distance to enemy < 3:\n"
+        "    player attack\n";
+
+    Lexer lexer(source, "sensors.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+    GS_ASSERT_EQ(program->getStatements().size(), 2);
+
+    auto* firstIf = dynamic_cast<IfStmt*>(program->getStatements()[0].get());
+    GS_ASSERT(firstIf != nullptr);
+
+    auto* obstacle = dynamic_cast<SensoryConditionExpr*>(firstIf->getCondition());
+    GS_ASSERT(obstacle != nullptr);
+    GS_ASSERT_EQ(obstacle->getSensoryType(), SensoryType::ObstacleAhead);
+
+    auto* secondIf = dynamic_cast<IfStmt*>(program->getStatements()[1].get());
+    GS_ASSERT(secondIf != nullptr);
+
+    auto* comparison = dynamic_cast<BinaryExpr*>(secondIf->getCondition());
+    GS_ASSERT(comparison != nullptr);
+    GS_ASSERT_EQ(comparison->getOp(), BinaryOp::Less);
+
+    auto* distanceSensor =
+        dynamic_cast<SensoryConditionExpr*>(comparison->getLeft());
+    GS_ASSERT(distanceSensor != nullptr);
+    GS_ASSERT_EQ(
+        distanceSensor->getSensoryType(),
+        SensoryType::DistanceToEnemy);
+}
+
+GS_TEST(ParserTests, FunctionArgumentsAndParenthesizedExpression) {
+    std::string source =
+        "function patrol(distance, angle):\n"
+        "    player move forward distance\n"
+        "    player turn right angle\n"
+        "call patrol(5, 90)\n"
+        "set result = (2 + 3) * 4\n";
+
+    Lexer lexer(source, "functions.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+    GS_ASSERT_EQ(program->getStatements().size(), 3);
+
+    auto* function =
+        dynamic_cast<FunctionDeclStmt*>(program->getStatements()[0].get());
+    GS_ASSERT(function != nullptr);
+    GS_ASSERT_EQ(function->getName(), "patrol");
+    GS_ASSERT_EQ(function->getParams().size(), 2);
+    GS_ASSERT_EQ(function->getParams()[0], "distance");
+    GS_ASSERT_EQ(function->getParams()[1], "angle");
+
+    auto* call =
+        dynamic_cast<FunctionCallStmt*>(program->getStatements()[1].get());
+    GS_ASSERT(call != nullptr);
+    GS_ASSERT_EQ(call->getName(), "patrol");
+    GS_ASSERT_EQ(call->getArgs().size(), 2);
+
+    auto* result =
+        dynamic_cast<VarDeclStmt*>(program->getStatements()[2].get());
+    GS_ASSERT(result != nullptr);
+
+    auto* multiply =
+        dynamic_cast<BinaryExpr*>(result->getInitializer());
+    GS_ASSERT(multiply != nullptr);
+    GS_ASSERT_EQ(multiply->getOp(), BinaryOp::Multiply);
+}
