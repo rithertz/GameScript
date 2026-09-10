f532ca6762b62de958f20611cfe3537a4528f84c
Rithwik <trixterzmusic@gmail.com>
2026-09-11 00:37:28 +0530

    test: expand AST and runtime coverage

--- tests/unit/ASTTests.cpp
+++ tests/unit/ASTTests.cpp
@@ -6,7 +6,7 @@
 using namespace gamescript;
 
 GS_TEST(ASTTests, ASTPrettyPrinterFormat) {
-    std::string source = 
+    std::string source =
         "set distance = 5\n"
         "player move forward distance\n"
         "if enemy nearby:\n"
@@ -15,6 +15,7 @@ GS_TEST(ASTTests, ASTPrettyPrinterFormat) {
     Lexer lexer(source, "ast_test.gs");
     Parser parser(lexer.tokenize());
     auto program = parser.parseProgram();
+
     GS_ASSERT(program != nullptr);
 
     ASTPrinter printer;
@@ -25,5 +26,110 @@ GS_TEST(ASTTests, ASTPrettyPrinterFormat) {
     GS_ASSERT(output.find("VarDecl: distance") != std::string::npos);
     GS_ASSERT(output.find("Move: FORWARD") != std::string::npos);
     GS_ASSERT(output.find("EnemyNearby") != std::string::npos);
+    GS_ASSERT(output.find("Action: ATTACK") != std::string::npos);
+}
+
+GS_TEST(ASTTests, ExpressionNodes) {
+    std::string source =
+        "set distance = 5 + 3 * 2\n";
+
+    Lexer lexer(source, "ast_expression.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+
+    ASTPrinter printer;
+    std::string output = printer.print(*program);
+
+    GS_ASSERT(output.find("VarDecl: distance") != std::string::npos);
+    GS_ASSERT(output.find("Value: (5 + (3 * 2))") != std::string::npos);
+}
+
+GS_TEST(ASTTests, IfElseTree) {
+    std::string source =
+        "if health low:\n"
+        "    player defend\n"
+        "else:\n"
+        "    player attack\n";
+
+    Lexer lexer(source, "ast_if_else.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+
+    ASTPrinter printer;
+    std::string output = printer.print(*program);
+
+    GS_ASSERT(output.find("If") != std::string::npos);
+    GS_ASSERT(output.find("HealthLow") != std::string::npos);
+    GS_ASSERT(output.find("Action: DEFEND") != std::string::npos);
+    GS_ASSERT(output.find("Action: ATTACK") != std::string::npos);
+}
+
+GS_TEST(ASTTests, LoopNodes) {
+    std::string source =
+        "repeat 3:\n"
+        "    player move forward 1\n"
+        "while health > 0:\n"
+        "    player defend\n";
+
+    Lexer lexer(source, "ast_loops.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+
+    ASTPrinter printer;
+    std::string output = printer.print(*program);
+
+    GS_ASSERT(output.find("Repeat") != std::string::npos);
+    GS_ASSERT(output.find("While") != std::string::npos);
+    GS_ASSERT(output.find("Move: FORWARD") != std::string::npos);
+    GS_ASSERT(output.find("Action: DEFEND") != std::string::npos);
+}
+
+GS_TEST(ASTTests, FunctionAndCallTree) {
+    std::string source =
+        "function patrol(distance, angle):\n"
+        "    player move forward distance\n"
+        "    player turn left angle\n"
+        "call patrol(5, 90)\n";
+
+    Lexer lexer(source, "ast_function.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+
+    ASTPrinter printer;
+    std::string output = printer.print(*program);
+
+    GS_ASSERT(output.find("Function: patrol") != std::string::npos);
+    GS_ASSERT(output.find("distance") != std::string::npos);
+    GS_ASSERT(output.find("angle") != std::string::npos);
+    GS_ASSERT(output.find("Move: FORWARD") != std::string::npos);
+    GS_ASSERT(output.find("Turn: LEFT") != std::string::npos);
+    GS_ASSERT(output.find("Call: patrol") != std::string::npos);
+}
+
+GS_TEST(ASTTests, BooleanAndSensorTree) {
+    std::string source =
+        "if enemy nearby and not health low:\n"
+        "    player attack\n";
+
+    Lexer lexer(source, "ast_boolean.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    GS_ASSERT(program != nullptr);
+
+    ASTPrinter printer;
+    std::string output = printer.print(*program);
+
+    GS_ASSERT(output.find("EnemyNearby") != std::string::npos);
+    GS_ASSERT(output.find("HealthLow") != std::string::npos);
+    GS_ASSERT(output.find("Condition: (EnemyNearby and (not HealthLow))") != std::string::npos);
     GS_ASSERT(output.find("Action: ATTACK") != std::string::npos);
 }
--- tests/unit/RuntimeTests.cpp
+++ tests/unit/RuntimeTests.cpp
@@ -77,3 +77,151 @@ GS_TEST(RuntimeTests, EndToEndProgramVMExecution) {
     GS_ASSERT_EQ(world.getPlayer().facing, Direction::East);
     GS_ASSERT(!world.getActionLog().empty());
 }
+
+GS_TEST(RuntimeTests, MovementDirectionsAndBoundaries) {
+    GameWorld world(5, 5);
+    world.setPlayerPosition(2, 2, Direction::North);
+
+    // Forward: North
+    GS_ASSERT(world.movePlayer(0, 1));
+    GS_ASSERT_EQ(world.getPlayer().x, 2);
+    GS_ASSERT_EQ(world.getPlayer().y, 1);
+
+    // Right relative to North: East
+    GS_ASSERT(world.movePlayer(3, 1));
+    GS_ASSERT_EQ(world.getPlayer().x, 3);
+    GS_ASSERT_EQ(world.getPlayer().y, 1);
+
+    // Backward relative to North: South
+    GS_ASSERT(world.movePlayer(1, 1));
+    GS_ASSERT_EQ(world.getPlayer().x, 3);
+    GS_ASSERT_EQ(world.getPlayer().y, 2);
+
+    // Move beyond boundary should stop at edge
+    world.setPlayerPosition(0, 0, Direction::North);
+    GS_ASSERT(!world.movePlayer(0, 1));
+    GS_ASSERT_EQ(world.getPlayer().x, 0);
+    GS_ASSERT_EQ(world.getPlayer().y, 0);
+}
+
+GS_TEST(RuntimeTests, ObstacleCollision) {
+    GameWorld world(10, 10);
+    world.setPlayerPosition(3, 3, Direction::East);
+    world.addObstacle(4, 3);
+
+    GS_ASSERT(world.isObstacleAhead());
+
+    bool moved = world.movePlayer(0, 2);
+
+    GS_ASSERT(!moved);
+    GS_ASSERT_EQ(world.getPlayer().x, 3);
+    GS_ASSERT_EQ(world.getPlayer().y, 3);
+}
+
+GS_TEST(RuntimeTests, PlayerActions) {
+    GameWorld world(10, 10);
+    world.setPlayerPosition(5, 5, Direction::North);
+
+    GS_ASSERT(world.playerDefend());
+    GS_ASSERT(world.getPlayer().isDefending);
+
+    GS_ASSERT(world.playerJump());
+    GS_ASSERT_EQ(world.getPlayer().y, 3);
+
+    GS_ASSERT(world.playerInteract() == false);
+
+    GS_ASSERT(!world.getActionLog().empty());
+}
+
+GS_TEST(RuntimeTests, ObjectiveInteraction) {
+    GameWorld world(10, 10);
+    world.setObjective(4, 4);
+    world.setPlayerPosition(4, 4, Direction::North);
+
+    GS_ASSERT(world.playerInteract());
+    GS_ASSERT(world.getObjective().isCompleted);
+
+    GS_ASSERT(!world.getActionLog().empty());
+}
+
+GS_TEST(RuntimeTests, HealthSensorThreshold) {
+    GameWorld world(10, 10);
+    world.setPlayerPosition(2, 2);
+
+    world.getPlayer().hp = 100;
+    GS_ASSERT(!world.isHealthLow());
+
+    world.getPlayer().hp = 30;
+    GS_ASSERT(world.isHealthLow());
+
+    world.getPlayer().hp = 10;
+    GS_ASSERT(world.isHealthLow());
+
+    GS_ASSERT(world.isHealthLow(10));
+    GS_ASSERT(!world.isHealthLow(9));
+}
+
+GS_TEST(RuntimeTests, EnemyDistanceAndDeadEnemySensor) {
+    GameWorld world(10, 10);
+    world.setPlayerPosition(2, 2);
+    world.addEnemy(2, 5, 40, "Goblin");
+    world.addEnemy(8, 8, 40, "Orc");
+
+    GS_ASSERT_EQ(world.getDistanceToClosestEnemy(), 3);
+    GS_ASSERT(world.isEnemyNearby(3));
+    GS_ASSERT(!world.isEnemyNearby(2));
+
+    world.getEnemies()[0].isAlive = false;
+
+    GS_ASSERT_EQ(world.getDistanceToClosestEnemy(), 12);
+    GS_ASSERT(!world.isEnemyNearby(3));
+}
+
+GS_TEST(RuntimeTests, ConditionalSensorExecution) {
+    std::string source =
+        "if enemy nearby:\n"
+        "    player attack\n"
+        "else:\n"
+        "    player move forward 2\n";
+
+    Lexer lexer(source, "sensor_runtime.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    ir::IRBuilder builder;
+    auto module = builder.build(*program);
+
+    GameWorld world(10, 10);
+    world.setPlayerPosition(2, 5, Direction::North);
+    world.addEnemy(2, 4, 30, "Goblin");
+
+    VirtualMachine vm(world);
+    GS_ASSERT(vm.execute(*module));
+
+    // Enemy is nearby and directly ahead, so the attack branch should run.
+    GS_ASSERT_EQ(world.getEnemies()[0].hp, 5);
+    GS_ASSERT_EQ(world.getPlayer().y, 5);
+}
+
+GS_TEST(RuntimeTests, RepeatLoopExecution) {
+    std::string source =
+        "repeat 3:\n"
+        "    player move forward 1\n";
+
+    Lexer lexer(source, "repeat_runtime.gs");
+    Parser parser(lexer.tokenize());
+    auto program = parser.parseProgram();
+
+    ir::IRBuilder builder;
+    auto module = builder.build(*program);
+
+    GameWorld world(10, 10);
+    world.setPlayerPosition(2, 5, Direction::North);
+
+    VirtualMachine vm(world);
+    GS_ASSERT(vm.execute(*module));
+
+    GS_ASSERT_EQ(world.getPlayer().x, 2);
+    GS_ASSERT_EQ(world.getPlayer().y, 2);
+    GS_ASSERT_EQ(world.getActionLog().size(), static_cast<size_t>(3));
+}
