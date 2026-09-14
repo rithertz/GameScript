bda24dd71a412a75214277786a68dc3dd6ca9f62
Dheeraj <dheerajvaliveti240@gmail.com>
2026-09-14 15:20:18 +0530

    Show CFG edges in basic block dumps

--- src/ir/BasicBlock.cpp
+++ src/ir/BasicBlock.cpp
@@ -6,6 +6,25 @@ namespace gamescript::ir {
 std::string BasicBlock::toString() const {
     std::ostringstream oss;
     oss << name_ << ":\n";
+
+    oss << "  Predecessors: [";
+    for (std::size_t i = 0; i < predecessors_.size(); ++i) {
+        if (i > 0) {
+            oss << ", ";
+        }
+        oss << predecessors_[i];
+    }
+    oss << "]\n";
+
+    oss << "  Successors: [";
+    for (std::size_t i = 0; i < successors_.size(); ++i) {
+        if (i > 0) {
+            oss << ", ";
+        }
+        oss << successors_[i];
+    }
+    oss << "]\n";
+
     for (const auto& inst : instructions_) {
         oss << inst.toString() << "\n";
     }
